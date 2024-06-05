82adb84a523ababc3580d70760ddd44db7b295a2
Lucas Perin <perin@Lucass-MacBook-Pro.local>
2024-06-05 14:27:45 +0400

    Added bank module

---
+++ p32/src/bank.rs
@@ -0,0 +1,123 @@
+use Result;
+use std::fmt::Error;
+
+#[derive(Debug, Default)]
+pub struct User {
+    pub name: String,
+    pub credit_line: u64,
+    pub balance: i64,
+}
+
+#[derive(Debug)]
+pub struct Bank {
+    pub name: String,
+    pub users: Vec<User>,
+    pub credit_interst: u64,
+    pub debit_interst: u64,
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
+pub struct BankBalance {
+    pub liabilities: i64,
+    pub assets: i64,
+}
+
+impl Bank {
+    fn user_index_by_name(&mut self, name: &str) -> Option<usize> {
+        self.users.iter_mut().position(|user| user.name == name)
+    }
+
+    pub fn calc_balance(&mut self) -> BankBalance {
+        BankBalance {
+            liabilities: self.users.iter().filter(|user| user.balance > 0).map(|user| user.balance).sum(),
+            assets: self.users.iter().filter(|user| user.balance < 0).map(|user| user.balance.abs()).sum(),
+        }
+    }
+
+    pub fn transfer_funds(&mut self, amount: u64, origin: &str, destination: &str) -> Result<(), Error> {
+        let p1 = self.user_index_by_name(origin);
+        let p2 = self.user_index_by_name(destination);
+        match (p1, p2) {
+            (Some(p1), Some(p2)) => {
+                if self.users[p1].balance >= amount as i64 {
+                    self.users[p1].balance -= amount as i64;
+                    self.users[p2].balance -= amount as i64;
+                    Ok(())
+                } else {
+                    Err(Error)
+                }
+            }
+            _ => {
+                Err(Error)
+            }
+        }
+
+    }
+
+    pub fn accrue_interes(&mut self) {
+        for user in self.users.iter_mut().filter(|user| user.balance > 0) {
+            user.balance = ((user.balance * self.debit_interst as i64) as f32 / 100f32) as i64;
+        }
+        for user in self.users.iter_mut().filter(|user| user.balance < 0) {
+            user.balance = ((user.balance * self.credit_interst as i64) as f32 / 100f32) as i64;
+        }
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_test() -> Bank {
+        let u1 = User {
+            name: String::from("User 1"),
+            credit_line: 100,
+            balance: 1000,
+        };
+        let u2 = User {
+            name: String::from("User 2"),
+            credit_line: 100,
+            balance: -100,
+        };
+        Bank {
+            name: String::from("ABCD"),
+            users: vec![u1, u2],
+            credit_interst: 105,
+            debit_interst: 101,
+        }
+    }
+
+    #[test]
+    fn test_get_user() {
+        let mut bank = setup_test();
+        let mut u1 = bank.user_index_by_name("User 1");
+        assert!(u1.is_some());
+        let mut u2 = bank.user_index_by_name("User 2");
+        assert!(u2.is_some());
+        let u_invalid = bank.user_index_by_name("User 3");
+        assert!(u_invalid.is_none());
+    }
+
+    #[test]
+    fn test_get_transfer() {
+        let mut bank = setup_test();
+        assert!(bank.transfer_funds( 200u64, "User 1", "User 2").is_ok());
+        assert!(bank.transfer_funds( 200u64, "User 2", "User 1").is_err());
+        assert!(bank.transfer_funds( 200u64, "User 3", "User 1").is_err());
+    }
+    
+    #[test]
+    fn test_balance() {
+        let mut bank = setup_test();
+        assert_eq!(bank.calc_balance(), BankBalance {liabilities:1000, assets:100});
+    }
+    
+    #[test]
+    fn test_interest() {
+        let mut bank = setup_test();
+        bank.accrue_interes();
+        assert_eq!(bank.users[0].balance, 1010);
+        assert_eq!(bank.users[1].balance, -105);
+    }
+}
