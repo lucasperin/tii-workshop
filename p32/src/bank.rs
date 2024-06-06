a917513f2c90a90fb9101f4bebfc79c6f840928d
Lucas Perin <perin@Lucass-MacBook-Pro.local>
2024-06-06 15:13:40 +0400

    Changed bank vector to hashmap.

--- p32/src/bank.rs
+++ p32/src/bank.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+use std::ops::Neg;
 use Result;
 
 pub enum Error {
     NotEnoughFunds,
     UserDoesNotExists,
+    BadConversion,
+    Overflow,
 }
 
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
@@ -15,7 +19,7 @@ pub struct User {
 #[derive(Debug)]
 pub struct Bank {
     pub name: String,
-    pub users: Vec<User>,
+    pub users: HashMap<String, User>,
     pub credit_interst: u64,
     pub debit_interst: u64,
 }
@@ -27,24 +31,39 @@ pub struct BankBalance {
 }
 
 impl Bank {
-    fn user_index_by_name(&mut self, name: &str) -> Option<usize> {
-        self.users.iter_mut().position(|user| user.name == name)
-    }
+    // fn user_index_by_name(&mut self, name: &str) -> Option<usize> {
+    //     self.users.
+    //     self.users.iter_mut().position(|user| user.name == name)
+    // }
 
     pub fn calc_balance(&mut self) -> BankBalance {
         BankBalance {
             liabilities: self
                 .users
                 .iter()
-                .filter(|user| user.balance > 0)
-                .map(|user| user.balance.unsigned_abs())
+                .filter(|(_, user)| user.balance > 0)
+                .map(|(_, user)| user.balance.unsigned_abs())
                 .sum(),
             assets: self
                 .users
                 .iter()
-                .filter(|user| user.balance < 0)
-                .map(|user| user.balance.unsigned_abs())
+                .filter(|(_, user)| user.balance < 0)
+                .map(|(_, user)| user.balance.unsigned_abs())
                 .sum(),
+        }
+    }
+
+    fn add_to_balance(&mut self, origin: &str, amount: i64) -> Result<(), Error> {
+        let user = self.users.get_mut(origin);
+        match user {
+            Some(user) => match user.balance.checked_add(amount) {
+                Some(new_balance) => {
+                    user.balance = new_balance;
+                    Ok(())
+                }
+                None => Err(Error::Overflow),
+            },
+            None => Err(Error::UserDoesNotExists),
         }
     }
 
@@ -54,13 +73,12 @@ impl Bank {
         origin: &str,
         destination: &str,
     ) -> Result<(), Error> {
-        let p1 = self.user_index_by_name(origin);
-        let p2 = self.user_index_by_name(destination);
-        match (p1, p2) {
-            (Some(p1), Some(p2)) => {
-                if self.users[p1].balance >= amount as i64 {
-                    self.users[p1].balance -= amount as i64;
-                    self.users[p2].balance += amount as i64;
+        match (self.users.get(origin), self.users.get(destination)) {
+            (Some(p1), Some(_)) => {
+                if p1.balance - amount as i64 >= (p1.credit_line as i64).neg() {
+                    let amount: i64 = amount.try_into().map_err(|_| Error::BadConversion)?;
+                    self.add_to_balance(origin, amount.neg())?;
+                    self.add_to_balance(destination, amount)?;
                     Ok(())
                 } else {
                     Err(Error::NotEnoughFunds)
@@ -70,23 +88,23 @@ impl Bank {
         }
     }
 
-    pub fn accrue_interes(&mut self) {
-        for user in self.users.iter_mut().filter(|user| user.balance > 0) {
+    pub fn accrue_interest(&mut self) {
+        for (_, user) in self.users.iter_mut().filter(|(_, user)| user.balance > 0) {
             user.balance += user.balance * self.debit_interst as i64 / 10_000;
         }
-        for user in self.users.iter_mut().filter(|user| user.balance < 0) {
+        for (_, user) in self.users.iter_mut().filter(|(_, user)| user.balance < 0) {
             user.balance += user.balance * self.credit_interst as i64 / 10_000;
         }
     }
 
     pub fn merge_bank(&mut self, bank: Bank) {
-        for user in bank.users {
-            match self.user_index_by_name(user.name.as_str()) {
-                Some(position) => {
-                    self.users[position].balance += user.balance;
+        for (name, user) in bank.users {
+            match self.users.get_mut(&name) {
+                Some(existing_user) => {
+                    existing_user.balance += user.balance;
                 }
                 None => {
-                    self.users.push(user);
+                    self.users.insert(name, user);
                 }
             }
         }
@@ -110,28 +128,18 @@ mod tests {
         };
         Bank {
             name: String::from("ABCD"),
-            users: vec![u1, u2],
+            users: HashMap::from([(u1.name.to_string(), u1), (u2.name.to_string(), u2)]),
             credit_interst: 500,
             debit_interst: 100,
         }
     }
 
     #[test]
-    fn test_get_user() {
-        let mut bank = setup_test();
-        let u1 = bank.user_index_by_name("User 1");
-        assert!(u1.is_some());
-        let u2 = bank.user_index_by_name("User 2");
-        assert!(u2.is_some());
-        let u_invalid = bank.user_index_by_name("User 3");
-        assert!(u_invalid.is_none());
-    }
-
-    #[test]
     fn test_get_transfer() {
         let mut bank = setup_test();
         assert!(bank.transfer_funds(200u64, "User 1", "User 2").is_ok());
-        assert!(bank.transfer_funds(200u64, "User 2", "User 1").is_err());
+        assert!(bank.transfer_funds(150u64, "User 2", "User 1").is_ok());
+        assert!(bank.transfer_funds(300u64, "User 2", "User 1").is_err());
         assert!(bank.transfer_funds(200u64, "User 3", "User 1").is_err());
     }
 
@@ -150,9 +158,9 @@ mod tests {
     #[test]
     fn test_interest() {
         let mut bank = setup_test();
-        bank.accrue_interes();
-        assert_eq!(bank.users[0].balance, 1010);
-        assert_eq!(bank.users[1].balance, -105);
+        bank.accrue_interest();
+        assert_eq!(bank.users["User 1"].balance, 1010);
+        assert_eq!(bank.users["User 2"].balance, -105);
     }
 
     #[test]
@@ -164,10 +172,10 @@ mod tests {
             credit_line: 100,
             balance: 1,
         };
-        bank2.users.push(u3.clone());
+        bank2.users.insert(u3.name.to_string(), u3.clone());
         bank.merge_bank(bank2);
-        assert_eq!(bank.users[2], u3);
-        assert_eq!(bank.users[0].balance, 2000);
-        assert_eq!(bank.users[1].balance, -200);
+        assert_eq!(bank.users["User 3"], u3);
+        assert_eq!(bank.users["User 1"].balance, 2000);
+        assert_eq!(bank.users["User 2"].balance, -200);
     }
 }
