5c8061ce2ff09e61a90160e89563766dd3512264
Lucas Perin <perin@Lucass-MacBook-Pro.local>
2024-06-13 11:03:19 +0400

    Fix build script warning.

--- project/build.rs
+++ project/build.rs
@@ -7,11 +7,16 @@ fn main() {
 
     let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let gen = cbindgen::generate(crate_dir);
-    if gen.is_ok() {
-        gen.unwrap()
-            .write_to_file(out_path.join("src/api/crypto_api.h"));
-    } else {
-        println!("CBINDGEN Error: {:?},", Some(gen.err()));
-        panic!("Bindgen error");
+    match gen {
+        Err(e) => {
+            println!("CBINDGEN Error: {:?},", e);
+            panic!("Bindgen error");
+        }
+        Ok(bindings) => {
+            bindings
+                .write_to_file(
+                    out_path.join("src/api/crypto_api.h")
+                );
+        }
     }
 }
